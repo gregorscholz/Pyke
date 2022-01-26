0826e96c2ae1d65a58b7db0fb442d4d56a6cd690
SchGre <gregorscholz01@gmail.com>
2022-01-26 23:19:30 +0100

    first commit

---
+++ Pyke.py
@@ -0,0 +1,13 @@
+class Pyke():    
+    
+    token = None
+    region = None
+    
+    # Sets the token
+    def set_token(token: str):
+        Pyke.token = token
+    
+    # Sets the region
+    def set_region(region: str):
+        Pyke.region = region   
+        
